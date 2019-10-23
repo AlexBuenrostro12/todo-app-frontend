99a80baf10efc24fed70ffd06da62b47084a23c0
Alex Buenrostro <alex101211b@gmail.com>
2019-10-22 23:44:30 -0500

    Connection with the backend created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Layout from './hoc/Layout';
+import ApolloClient, { gql } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 
-function App() {
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+});
+
+client.query({
+  query: gql`
+      {
+        users {
+          name
+        }
+      }
+    `
+}).then(res => console.log(res)).catch(err => {console.log(err)});
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ApolloProvider client={client}>
+          <Layout />
+        </ApolloProvider>
     </div>
   );
 }
---
+++ src/hoc/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Auxiliar from './Auxiliar';
+
+
+const Layout = (props) => (
+  <Auxiliar>
+      <h1>Hello</h1>
+  </Auxiliar>
+);
+
+export default Layout;
